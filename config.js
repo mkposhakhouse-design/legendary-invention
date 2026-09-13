bcee7442404ae551e5f869e38221c07b82507106
mkposhakhouse-design <mkposhakhouse@gmail.com>
2026-09-13 15:18:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Public Supabase browser configuration. Access is protected by Auth and RLS.
+window.MK_CONFIG = {
+  url: 'https://hwmwvfnbuheymefedbzo.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3bXd2Zm5idWhleW1lZmVkYnpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyODUwMjEsImV4cCI6MjEwNDg2MTAyMX0.jsRF-5nWgZA61Ft0UKojx-Kg2FMUbSHldaRcyzkrZzI'
+};
